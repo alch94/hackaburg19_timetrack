2ad431ec4ee0f35edee97fbef6152368f6e08c21
markusmo3 <markusmo3@zoho.com>
2019-05-24 17:15:25 +0200

    Initial commit!

---
+++ frontend/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <chart></chart>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import chart from './components/chart.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    chart
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
